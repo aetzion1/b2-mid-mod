9a868df7cffc3d483b6184ed2be01e6ab3f3fb0a
Adam Etzion <adametzion90@gmail.com>
2021-01-04 10:11:12 -0700

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'movies/index'
-  get 'movies/show'
-  get 'studios/index'
+  get '/studios', to:'studios#index'
+  get '/movies/:id', to:'movies#show'
+  get '/actors/:id', to:'actors#show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
