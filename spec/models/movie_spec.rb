f9f7dbcfc69bfe642568a6f344919835fb66c65b
Adam Etzion <adametzion90@gmail.com>
2021-01-04 09:42:54 -0700

    Update actor and movie model rspec

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -4,5 +4,6 @@ RSpec.describe Movie, type: :model do
   describe "relationships" do
     it { should belong_to :studio}
     it { should have_many :movie_actors}
+    it { should have_many(:actors).through(:movie_actors)}
   end
 end
